5e80580ae70647455ac01f4678e1cc88eb252d63
Maynard Hartman <mjhartman@brighthouse.com>
2019-09-04 23:36:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -68,14 +68,26 @@ def monopoly_with_fourth_tier
   # complexity.
   monopoly = { 
       :railroads => {
-        :piece => 4 {
-          
-        
+        :pieces => 4, {
         :rent_in_dollars => {
-               
+          :one_piece_owned => 25,
+          :two_pieces_owned => 50,
+          :three_pieces_owned => 100,
+          :four_pieces_owned => 200
         }
         :names => {
-          
+          :reading_railroad => {
+             "mortgage_value"=>"$100" 
+          }
+          :pennsylvania_railroad => {
+            "mortgage_value"=>"$200"  
+          }
+          :pennsylvania_railroad=> {
+            "mortgage_value"=>"$400"  
+          }
+          :shortline => {
+            "mortgage_value"=>"$800"
+          }
         }
       }  
     }
