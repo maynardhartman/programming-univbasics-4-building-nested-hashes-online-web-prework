f4accf6e22876ba2865370ec4659f0caa6f934be
Maynard Hartman <mjhartman@brighthouse.com>
2019-09-04 23:23:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,10 +14,9 @@ def monopoly_with_second_tier
   {
     monopoly = { 
       :railroads => {
-        :pieces => 4 {
-          
-        
+        :pieces => 4, {
         :rent_in_dollars => {
+          
                
         }
         :names => {}
