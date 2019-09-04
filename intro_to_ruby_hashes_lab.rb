ecf1d6f12abca39365d13a88670f6276e588ede7
Maynard Hartman <mjhartman@brighthouse.com>
2019-09-04 23:32:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,9 +23,11 @@ def monopoly_with_second_tier
         }
         :names => {
           :reading_railroad => {
+             "mortgage_value"=>"$100" 
+          }
+          :pennsylvania_railroad => {
             
           }
-          :pennsylvania_railroad => {}
           :pennsylvania_railroad=> {}
           :shortline => {}
         }
